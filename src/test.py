8038d33f6ded033d7c874626b86803f1e4728a47
[공부하는 회사원] 빨강달 <57031547+everlearningemployee@users.noreply.github.com>
2020-02-27 23:34:03 +0900

    Update test.py

--- src/test.py
+++ src/test.py
@@ -2,90 +2,78 @@ import json
 import requests
 import math
 import decimal
+from calc import *
 
 
-def dcml(f):
-    return decimal.Decimal(str(f))
-
-
-def calcBuyOrder(balanceRate,
-                 orderTrigeringPriceChagePercentPoint,
-                 filledPrice,
-                 coinAmount,
-                 cashValue,
-                 order_min_size,
-                 tick_size,
-                 min_price,
-                 **kwargs):
-    b = dcml(balanceRate)  # 밸런싱비율
-    t = dcml(orderTrigeringPriceChagePercentPoint)  # 주문트리거링가격변동포인트
-    pf = dcml(filledPrice)  # 최근내주문체결가
-    ai = dcml(coinAmount)  # 코인보유량
-    vc = dcml(cashValue)  # 주문금액
-    am = dcml(order_min_size)  # 최소주문량
-    ts = dcml(tick_size)  # 호가단위
-
-    po = pf * (1 - t)
-    ao = (b * vc - po * ai) / (po * (1 + b))
-    # 최소주문량보다 작을 경우
-    if ao < am:
-        ao = am
-        po = b * vc / (ai + am * (1 + b))
-    # 호가단위로 보정
-    po = math.floor(po / ts) * ts
-    # 최소주문가보다 작을 경우
-    if po < int(min_price):
-        print("안사: %s, %s" % (ao, po))
-        po, ao = 0, 0
-    return {"price": float(po), "amount": float(ao)}
-
-
-def calcSellOrder(balanceRate,
-                  orderTrigeringPriceChagePercentPoint,
-                  filledPrice,
-                  coinAmount,
-                  cashValue,
-                  order_min_size,
-                  tick_size,
-                  min_price,
-                 **kwargs):
-    b = dcml(balanceRate)  # 밸런싱비율
-    t = dcml(orderTrigeringPriceChagePercentPoint)  # 주문트리거링가격변동포인트
-    pf = dcml(filledPrice)  # 최근내주문체결가
-    ai = dcml(coinAmount)  # 코인보유량
-    vc = dcml(cashValue)  # 주문금액
-    am = dcml(order_min_size)  # 최소주문량
-    ts = dcml(tick_size)  # 호가단위
-
-    po = pf * (1 + t)
-    ao = (po * ai - b * vc) / (po * (1 + b))
-    # 최소주문량보다 작을 경우
-    if ao < am:
-        ao = am
-        po = b * vc / (ai - am * (1 + b))
-    # 호가단위로 보정
-    po = math.ceil(po / ts) * ts
-    # 최소주문가보다 작을 경우
-    if po < int(min_price):
-        print("안팔아: %s, %s" % (ao, po))
-        po, ao = 0, 0
-    return {"price": float(po), "amount": float(ao)}
-
-
-env = {
+const = {
     "tick_size": 0.100000000000000000,
     "min_price": 10.000000000000000000,
     "max_price": 100000000,
     "order_min_size": 10.000000,
-    "order_max_size": 1000000.000000
+    "order_max_size": 1000000.000000,
+    'currency_pair': 'xrp_krw'
 }
 
+ticker = {
 
-def calcBalance(a, buyOrSell):
-    coinAmount = a['coinAmount']
-    cashValue = a['cashValue']
-    orderPrice = a['price']
-    orderAmount = a['amount']
+}
+
+prpnst = {
+    'distributionRate': 1,  # 배분비율
+    'triggeringFluctuations': 3  # = 0.03 # 주문 트리거링 가격변동%
+}
+
+filledOrdr = [
+    {
+        "timestamp": 1383707746000,
+        "completedAt": 1383797746000,
+        "id": "599",
+        "type": "sell",
+        "fee": {"currency": "krw", "value": "1500"},
+        "fillsDetail": {
+            "price": {"currency": "krw", "value": "1000000"},
+            "amount": {"currency": "btc", "value": "1"},
+            "native_amount": {"currency": "krw", "value": "1000000"},
+            "orderId": "1002"
+        }
+    },
+    {
+        "timestamp": 1383705741000,
+        "completedAt": 1383797746200,
+        "id": "597",
+        "type": "buy",
+        "fee": {"currency": "btc", "value": "0.0015"},
+        "fillsDetail": {
+            "price": {"currency": "krw", "value": "1000000"},
+            "amount": {"currency": "btc", "value": "1"},
+            "native_amount": {"currency": "krw", "value": "1000000"},
+            "orderId": "1002"
+        }
+    }
+]
+
+openOrdr = [
+    {
+        "timestamp": 1389173297000,
+        "id": "58726",
+        "type": "ask",
+        "price": {"currency": "krw", "value": "800000"},
+        "total": {"currency": "btc", "value": "1.00000000"},
+        "open": {"currency": "btc", "value": "0.75000000"}
+    },
+    {
+        "timestamp": 1386419377000,
+        "id": "37499",
+        "type": "bid",
+        "price": {"currency": "krw", "value": "700000"},
+        "total": {"currency": "btc", "value": "1.50000000"},
+        "open": {"currency": "btc", "value": "0.41200000"}
+    }
+]
+
+
+def calcBalance(coinAmount, cashValue, price, amount, buyOrSell, **kwargs):
+    '''검증용 함수'''
     try:
         vo = orderPrice * orderAmount
         if buyOrSell == "buy":
@@ -113,11 +101,11 @@ def testCalc():
 
     order = calcBuyOrder(**testData)
     print(order)
-    calcBalance(dict(**order, **testData), buyOrSell="buy")
+    calcBalance(**order, **testData, buyOrSell="buy")
 
     order = calcSellOrder(**testData)
     print(order)
-    calcBalance(dict(**order, **testData), buyOrSell="sell")
+    calcBalance(**order ** testData, buyOrSell="sell")
 
 
 def t1():
@@ -138,5 +126,25 @@ def t1():
     print(r.request.url)
     print(r.request.body)
 
+def f1(aa, bb, ff, **kwargs):
+    print(f'aa=[{aa}], bb=[{bb}], ff=[{ff}]')
 
-t1()
+def f2(**g):
+    f1(**g)
+
+def t2():
+    haha = {
+        'aa': 'a1111',
+        'dd': 'd1111'
+    }
+    hoho = {
+        'cc': 'c1111',
+        'bb': 'b1111'
+    }
+    hihi = {
+        'ee': 'ec1111',
+        'ff': 'f1111'
+    }
+    f2(**haha, **hoho, **hihi)
+
+t2()
