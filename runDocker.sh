24a0a39e0ad2c7f62037172fc673fd37f677d53a
빨강달 <reddiana@gmail.com>
2021-01-31 04:08:34 +0900

    쉬어가자

--- runDocker.sh
+++ runDocker.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 LEO=leo
-docker rm -f $LEO
-docker run -d \
-    --rm \
-    --name $LEO \
-    -v /home/ubuntu/leo/logs:/var/log/leo \
-    -v /home/ubuntu/leo/myorder:/leo/order \
-    leo
-docker logs -f $LEO
+cd /home/ubuntu/leo
+# docker-compose down
+docker-compose up -d
+docker-compose logs -f $LEO
 # sleep 3
 # watch -n 30 tail /home/ubuntu/leo/myorder/* /home/ubuntu/leo/logs/out.log  /home/ubuntu/leo/logs/order.log /home/ubuntu/leo/logs/trans.log
